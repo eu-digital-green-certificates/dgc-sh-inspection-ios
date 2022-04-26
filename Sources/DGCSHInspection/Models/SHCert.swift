81608217996e405dcbba9894706e54f69953a146
IgorKhomiak <ihorkhomyak@gmail.com>
2022-04-26 23:10:02 +0300

    removed extension to Core

--- Sources/DGCSHInspection/Models/SHCert.swift
+++ Sources/DGCSHInspection/Models/SHCert.swift
@@ -212,20 +212,3 @@ public class SHCert: CertificationProtocol, Codable {
         
     }
 }
-
-extension Data {
-    func inflateFixed() -> Data {
-        let size = 1024 * 10
-        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
-        defer { buffer.deallocate() }
-        return withUnsafeBytes { unsafeBytes in
-          let read = compression_decode_buffer(
-            buffer, size,
-            unsafeBytes.baseAddress!.bindMemory(to: UInt8.self, capacity: 1),
-            count, nil, COMPRESSION_ZLIB
-          )
-          return Data(bytes: buffer, count: read)
-        }
-    }
-}
-
