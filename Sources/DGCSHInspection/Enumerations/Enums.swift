57b9d7b653a24fd8579b720c000545d07c304d2c
IgorKhomiak <ihorkhomyak@gmail.com>
2022-04-27 13:36:32 +0300

    removed SH Error

--- Sources/DGCSHInspection/Enumerations/Enums.swift
+++ Sources/DGCSHInspection/Enumerations/Enums.swift
@@ -20,13 +20,3 @@ public enum SHCertType: String {
     }
 }
 
-public enum SHParsingError: Error {
-    case unknown
-    case invalidStructure
-    case kidNotIncluded
-    case kidNotFound(untrustedUrl: String)
-    case issuerNotIncluded
-    case timeBeforeNBF
-    case credentialExpired
-}
-
