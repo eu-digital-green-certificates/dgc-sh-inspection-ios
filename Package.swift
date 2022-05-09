8be748d6dd10f8d9110281ddb12c37a73d3d9e33
IgorKhomiak <ihorkhomyak@gmail.com>
2022-05-10 01:21:23 +0300

    change version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DGCSHInspection",
-	platforms: [.iOS(.v12), .macOS(.v10_15)],
+	platforms: [.iOS(.v12), .macOS(.v10_14)],
     
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
