4071789e5b52d42ba181b7ba7f1332159cd4ee10
Vitalii Koval <vitalii.koval@globallogic.com>
2022-05-17 17:20:54 +0300

    added certificateCreationDate property to SHCert

--- Sources/DGCSHInspection/Models/SHCert.swift
+++ Sources/DGCSHInspection/Models/SHCert.swift
@@ -14,6 +14,7 @@ import Compression
 import JOSESwift
 
 public class SHCert: CertificationProtocol, Codable {
+  public var certificateCreationDate: String
     public let certTypeString: String
 	public let cryptographicallyValid: Bool
 	public var isRevoked: Bool = false
